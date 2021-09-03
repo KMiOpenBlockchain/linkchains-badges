51a0efc99c5b0f84280059a99f52f003b17f2052
Pasquale Iero <pasquale.iero1@open.ac.uk>
2021-09-03 16:24:41 +0100

    Other badge methods added

--- main.js
+++ main.js
@@ -375,90 +375,30 @@ let detailsThree = {
 };
 
 
-//document.getElementById('inputOptions').innerHTML = "<pre>" + JSON.stringify(options, null, "  ") + "</pre>";
-
-let quads = "<http://bio2rdf.org/bio2rdf_dataset:bio2rdf-affymetrix-20121004> <http://www.w3.org/2000/01/rdf-schema#label> \"affymetrix dataset by Bio2RDF on 2012-10-04 [bio2rdf_dataset:bio2rdf-affymetrix-20121004]\"  .\n";
-    let editedQuads = "<http://bio3rdf.org/bio2rdf_dataset:bio2rdf-affymetrix-20121004> <http://www.w3.org/2000/01/rdf-schema#label> \"affymetrix dataset by Bio2RDF on 2012-10-04 [bio2rdf_dataset:bio2rdf-affymetrix-20121004]\"  .\n";
-
-document.getElementById('badgeData').innerHTML = "<pre id=\"badge\"></pre>";
-// document.getElementById('quads').innerText = quads;
-// document.getElementById('editedQuads').innerHTML = "<pre id=\"wrongQuads\"></pre>";
-// document.getElementById('wrongQuads').innerText = editedQuads;
-//
-// document.getElementById('granularMetadataOutput').innerHTML = "<pre id=\"granularMetadata\"></pre>";
-// document.getElementById('verificationOutput').innerHTML = "<pre id=\"verification\"></pre>";
-// document.getElementById('granularVerificationOutput').innerHTML = "<pre id=\"granularVerification\"></pre>";
-// document.getElementById('WrongVerificationOutput').innerHTML = "<pre id=\"WrongVerification\"></pre>";
-// document.getElementById('WrongGranularVerificationOutput').innerHTML = "<pre id=\"WrongGranularVerification\"></pre>";
-
 let linkchains_badges = window.linkchains_badges;
 let metadata = {};
-linkchains_badges.createSmartBadge(detailsOne).then((answer) => {
+
+
+const anchorData = async function (cfg, metadata) {
     var token = document.getElementById('token').value;
-
-    /* if (token !== '') {
-        metadata = fetch(anchorURL,{
-            method : 'POST',
+    var anchor = cfg.anchorURL;
+    // response from publish - the metadata
+    var anchorResponse = await axios.post(anchor, metadata,
+        {
             headers: {
-                'Content-Type': 'application/json',
-                'Authorization' : 'bearer ' + token
-            },
-            body : JSON.stringify(merkleTrees)
-        }).then((metadata) => {
-            console.log(JSON.stringify(metadata, null, "  ")); */
-    // var anchor = {
-    //     "type": "ETHMerQL",
-    //     "address": "0x714519C22e2E6278cC1CFba488cC800E9433AdEC",
-    //     "account": "0x1cf67BCCD5377aF1a69BadA19D699BCBa805E3f6",
-    //     "indexhash": "541b9b80c8bd5d222ad97082d47796177e3c938ffb82673865d9d78862543220",
-    //     "settings": {
-    //         "quadHash": "KECCAK-256",
-    //         "treeHash": "KECCAK-256",
-    //         "indexHash": "KECCAK-256",
-    //         "lsd": 2,
-    //         "indexType": "object",
-    //         "divisor": "0xa"
-    //     },
-    //     "transactionhash": "0xa8c35169e77e52744835a42a12d2c605c9f6c7a0a0ed8e146b493686812d2446"
-    // };
-    // merkleTrees.merkletrees.anchor = anchor;
-    // metadata = merkleTrees;
-    document.getElementById('answerData').innerHTML = "<pre>" + JSON.stringify(answer, null, "  ") + "</pre>";
- });
-//.then(() => {
-//     let granular = {};
-//     linkchains_badges.getGranularVerificationMetadata(quads, metadata, options).then((proofs) => {
-//         granular = proofs;
-//         document.getElementById('granularMetadata').innerText =  JSON.stringify(proofs, null, "  ");
-//     }).then(() => {
-//         let verificationWhole = {};
-//         linkchains_badges.verify(quads, metadata, config).then((results) => {
-//             verificationWhole = results;
-//             document.getElementById('verification').innerText =  JSON.stringify(verificationWhole, null, "  ");
-//         });
-//     })
-//      .then(() => {
-//         let verificationPartial = {};
-//         linkchains_badges.verify(quads, granular, config).then((resultsPartial) => {
-//             verificationPartial = resultsPartial;
-//             document.getElementById('granularVerification').innerText =  JSON.stringify(verificationPartial, null, "  ");
-//         });
-//     }).then(() => {
-//         let verificationWholeWrong = {};
-//         linkchains_badges.verify(editedQuads, metadata, config).then((resultsWrong) => {
-//             verificationWholeWrong = resultsWrong;
-//             document.getElementById('WrongVerification').innerText =  JSON.stringify(verificationWholeWrong, null, "  ");
-//         });
-//     }).then(() => {
-//         let verificationPartialWrong = {};
-//         linkchains_badges.verify(editedQuads, granular, config).then((resultsPartialWrong) => {
-//             verificationPartialWrong = resultsPartialWrong;
-//             document.getElementById('WrongGranularVerification').innerText =  JSON.stringify(verificationPartialWrong, null, "  ");
-//         });
-//     })
-//     ;
-// });
-//}});
+                'Authorization': token
+            }
+        });
+    var anchoredMetadata = anchorResponse.data;
+    return anchoredMetadata;
+};
 
-
-
+linkchains_badges.createSmartBadge(detailsOne).then((answerOne) => {
+    document.getElementById('answerDataOne').innerHTML = "<pre>" + JSON.stringify(answerOne, null, "  ") + "</pre>";
+    linkchains_badges.issueSmartBadge(cfg, detailsTwo, anchorData).then((answerTwo) => {
+        document.getElementById('answerDataTwo').innerHTML = "<pre>" + JSON.stringify(answerTwo, null, "  ") + "</pre>";
+        linkchains_badges.verifySmartBadge(cfg, detailsThree).then((answerThree) => {
+            document.getElementById('answerDataThree').innerHTML = "<pre>" + JSON.stringify(answerThree, null, "  ") + "</pre>";
+        });
+    })
+});
